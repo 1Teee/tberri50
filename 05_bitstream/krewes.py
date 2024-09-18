8c018a1244fddce9ba74fa063243db2992062746
Tawab <angrybirdsrock2007@gmail.com>
2024-09-18 19:47:23 -0400

    added krewes.txt

--- 05_bitstream/krewes.py
+++ 05_bitstream/krewes.py
@@ -20,9 +20,9 @@ while i < len(devos):
     dict[i] = devos[i]
     i+=1
     
-print(dict)
+#print(dict)
 n = random.randint(0, len(dict) -1)
-print(n)
+#print(n)
 
 devoinfo = dict[n].split("$$$")
 print("Period: " + devoinfo[0] + ", Name: " + devoinfo[1] + ", Ducky: " + devoinfo[2])
