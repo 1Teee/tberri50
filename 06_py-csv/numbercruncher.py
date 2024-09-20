e513210058a1ddea96cfd1f4486dc846c9910d81
Tawab <angrybirdsrock2007@gmail.com>
2024-09-19 20:02:46 -0400

    completed worK6

--- 06_py-csv/numbercruncher.py
+++ 06_py-csv/numbercruncher.py
@@ -1,8 +1,8 @@
 # The Duo-lingos -- Tawab Berri, Jacob Lukose, Jack Blair
 # SoftDev
 # K06 - Review on .csv's and accessing files, as well as the random library and dictionaries in Python.
-# 2024-09-18
-# time spent: 0:30
+# 2024-09-18 - 19
+# time spent: 0:50
 
 '''
 DISCO:
@@ -10,7 +10,13 @@ DISCO:
 QCC:
 ...
 HOW THIS SCRIPT WORKS:
-
+This script first splits up the data into both a 2d list and a dictionary. This is because a
+2d list is easier to work with because of its accessibility in terms of index and ability to store
+different types of data. The 2d list stores lists of length 2, which contain the occupation string, 
+and a percentage value which adds the percentage for the aforementioned occupation onto the 
+percentage in the previous list, to make the whole random selection probability system easier. 
+The script then creates a random integer representing a percentage values, and selects the 
+corresponding percentage value, then prints its respective occupation stored as a string.
 '''
 
 import random
@@ -18,10 +24,9 @@ f = open("occupations.csv", "r")
 text = f.read()
 # convert data from .csv to a string, which we will then manipulate and transform into a dictionary
 
-print(text)
+#print(text)
 
 dict = {}
-#pdict = {} # dictionary specific for percentage calculations
 rows = 22
 prob = [[0, ""]] * rows
 #print(prob)
@@ -39,14 +44,18 @@ while i < len(jobs) - 1:
         job = jobs[i].split("\"")
         dict[job[1]] = float(job[2][1:])
         prob[i] = [job[1], float(job[2][1:])]
-        if (i >= 1):
-            print(prob[i][1])
-            prob[i][1] = prob[i-1][1] + prob[i][1]
+    if (i > 0):
+        prob[i][1] = prob[i-1][1] + prob[i][1]
     i += 1
-    print(i)
     
 print(dict)
-print(prob)
+#print(prob)
 
-
-
+r = random.randint(0, 99)
+#print(r)
+ret = ""
+for job in prob:
+    if job[1] > r:
+        ret = job[0]
+        break
+print(ret)
