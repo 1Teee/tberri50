a768a9b55b9b26697944ff66c1d5eac1776d1245
Tawab <angrybirdsrock2007@gmail.com>
2024-10-15 20:17:11 -0400

    WORK16 FINALLY WORKS

--- 16_flask-sessions/app.py
+++ 16_flask-sessions/app.py
@@ -7,34 +7,40 @@ K15 - Analyzing the different forms of server responses of user inputs.
 time spent: 2.9 hrs
 '''
 
+# cited and adapted from victor casado's code
+
 from flask import Flask, render_template, request, session, redirect
 import os
 
 app = Flask(__name__)
 app.secret_key = os.urandom(32)
 
-logins = {"tawab" : "berri"}
-
-@app.route('/')
+@app.route('/', methods = ['GET', 'POST'])
 def login():
+    print('!!!')
     if 'username' in session:
+        print('!!')
         return redirect('/auth')
     else:
         return render_template("login.html")
     
-@app.route('/auth')
+@app.route('/auth', methods = ['GET', 'POST'])
 def auth_login():
     if 'username' in session:
-        return render_template("home.html", user = session['username'])
-    login = request.args
+        return render_template("response.html", user = session['username'])
+    if (request.method == 'POST'): login = request.form
+    else: login = request.args
+    print(login)
     username = login['username']
     password = login['password']
-    if username in logins:
-        if logins[username] == password:
-            session['username'] = username
-            return render_template("home.html", user = username)
-        else:
-            return redirect('/')
-    else:
-        return redirect('/')
-    
+    session['username'] = username
+    return render_template("response.html", user = username)
+
+@app.route('/logout', methods = ['GET', 'POST'])
+def logout():
+    del session['username']
+    return redirect('/')
+
+if __name__ == "__main__": #false if this file imported as module
+    app.debug = True
+    app.run()
