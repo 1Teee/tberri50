9730abae233b1956c3382697819742992a21fb72
Tawab <angrybirdsrock2007@gmail.com>
2024-10-19 15:32:39 -0400

    work19 doesnt work, teacher doesnt teach

--- 19_db0/build_db.py
+++ 19_db0/build_db.py
@@ -1,10 +1,15 @@
-#Clyde "Thluffy" Sinclair
-#SoftDev
-#skeleton/stub :: SQLITE3 BASICS
-#Oct 2024
-
+'''
+Tawab Berri
+The Duo-lingos - Tawab Berri, Jacob Lukose, Jack Blair
+SoftDev
+2024-10-18
+K19 - SQLite basics
+time spent: 2.8 hrs
+'''
 import sqlite3   #enable control of an sqlite database
 import csv       #facilitate CSV I/O
+
+import pprint as p
 
 
 DB_FILE="discobandit.db"
@@ -13,16 +18,34 @@ db = sqlite3.connect(DB_FILE) #open if file exists, otherwise create
 c = db.cursor()               #facilitate db ops -- you will use cursor to trigger db events
 
 #==========================================================
+students = []
+courses = []
+with open('students.csv', 'r') as f:
+    read = csv.DictReader(f)
+    for row in read:
+        students.append(row)
+        print(row)
 
+with open('courses.csv', 'r') as ftwo:
+    readtwo = csv.DictReader(ftwo)
+    for row in readtwo:
+        courses.append(row)
+p.pprint(students)
+p.pprint(courses)
 
-"""
-~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-< < < INSERT YOUR TEAM'S DB-POPULATING CODE HERE > > >
-~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-"""
+c.execute("CREATE TABLE students (code TEXT, mark INTEGER, id INTEGER PRIMARY KEY)")
+for i in students:
+    name = i['name']
+    age = int(i['age'])
+    id = int(i['id'])
+    c.execute(f"INSERT INTO students VALUES ({name}, {age}, {id})")
 
-command = ""          # test SQL stmt in sqlite3 shell, save as string
-c.execute(command)    # run SQL statement
+c.execute("CREATE TABLE courses (code TEXT, mark INTEGER, id INTEGER)")
+for i in courses:
+    code = i['code']
+    mark = int(i['mark'])
+    id = int(i['id'])
+    c.execute(f"INSERT INTO courses VALUES ({code}, {mark}, {id})")
 
 #==========================================================
 
