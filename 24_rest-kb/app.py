3d3dfa21f729259cafc3b5c0f4d9878d05e5b233
Tawab <angrybirdsrock2007@gmail.com>
2024-11-22 09:47:44 -0500

    added 24

---
+++ 24_rest-kb/app.py
@@ -0,0 +1,38 @@
+'''
+Mark Ma
+Topher - Mark, Tawab
+Softdev
+K23 - Rest API from Nasa
+2024-11-20
+time spent: 1
+'''
+
+from flask import Flask, render_template, request, session, redirect
+import urllib.request, urllib.parse
+import json
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def render():
+    secret_key = open("key_intelx.txt", "r").read().strip().rstrip()
+    url = "https://2.intelx.io/nycstudents.net/"
+    url += "&api_key=" + secret_key
+    url += "&camera=fhaz"
+    req = urllib.request.Request(url=url)
+
+    l = []
+    with urllib.request.urlopen(req) as response:
+        parsed_page = json.loads(response.read())
+        photos = parsed_page['photos']
+        for x in photos:
+            cam = x['camera']
+            name = x['camera']['full_name']
+            image_url = x['img_src']
+            l.append([image_url, name])
+
+    return render_template('main.html', images=l)
+
+if __name__ == "__main__":
+    app.run()
